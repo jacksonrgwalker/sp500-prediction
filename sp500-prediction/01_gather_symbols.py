038da8191204c0acf36505b7491c305de2c6b2f6
Jackson Walker <jacksonrgwalker@gmail.com>
2023-09-13 12:49:48 -0700

    keep earliest add, latest remove; fixed bugs

--- sp500-prediction/01_gather_symbols.py
+++ sp500-prediction/01_gather_symbols.py
@@ -4,48 +4,72 @@ from pathlib import Path
 # Grab table from Wikipedia
 url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 wiki_tables = pd.read_html(url)
-symbols = wiki_tables[0]
+symbols = wiki_tables[0].copy()
 
 # Clean up the table
-symbols.columns = symbols.columns.str.lower().str.replace(" ", "_", regex=False).str.replace("-", "_", regex=False)
+symbols.columns = (
+    symbols.columns.str.lower()
+    .str.replace(" ", "_", regex=False)
+    .str.replace("-", "_", regex=False)
+)
 symbols["symbol"] = symbols["symbol"].str.strip().str.replace(".", "-", regex=False)
-symbols['date_added'] = pd.to_datetime(symbols['date_added'], format='%Y-%m-%d', errors='coerce')
+symbols["date_added"] = pd.to_datetime(
+    symbols["date_added"], format="%Y-%m-%d", errors="coerce"
+)
 
-old_symbols = wiki_tables[1]
-old_symbols.columns = ['_'.join(set(c)) for c in old_symbols.columns.to_flat_index()]
+old_symbols = wiki_tables[1].copy()
+old_symbols.columns = [
+    "date",
+    "added_ticker",
+    "added_security",
+    "removed_ticker",
+    "removed_security",
+    "reason",
+]
 old_symbols.columns = old_symbols.columns.str.lower().str.replace(" ", "_", regex=False)
-old_symbols['date'] = pd.to_datetime(old_symbols['date'])
+old_symbols["date"] = pd.to_datetime(old_symbols["date"])
 
-removed = old_symbols[['removed_ticker', 'removed_security', 'date']].dropna()
-added = old_symbols[['added_ticker', 'added_security', 'date']].dropna()
-removed.columns = ['symbol', 'security', 'date_removed']
-added.columns = ['symbol', 'security', 'date_added']
+removed = old_symbols[["removed_ticker", "removed_security", "date"]].copy().dropna()
+added = old_symbols[["added_ticker", "added_security", "date"]].copy().dropna()
+removed.columns = ["symbol", "security", "date_removed"]
+added.columns = ["symbol", "security", "date_added"]
 
-# if symbol has > 1 date_removed, keep the earliest date removed
-removed = removed.sort_values(by=['date_removed']).drop_duplicates(subset=['symbol'], keep='first')
+
+# if symbol has > 1 date_removed, keep the latest date removed
+removed = removed.sort_values(by=["date_removed"]).drop_duplicates(
+    subset=["symbol"], keep="last"
+)
 
 # if symbol has > 1 date_added, keep the earliest date added
-added = added.sort_values(by=['date_added']).drop_duplicates(subset=['symbol'], keep='first')
+added = added.sort_values(by=["date_added"]).drop_duplicates(
+    subset=["symbol"], keep="first"
+)
 
-old_symbols = added.merge(removed, on=['symbol'], how='outer', suffixes=('', '_removed'))
-old_symbols['security'].fillna(old_symbols['security_removed'], inplace=True)
-old_symbols.drop(columns=['security_removed'], inplace=True)
+old_symbols = added.merge(
+    removed, on=["symbol"], how="outer", suffixes=("", "_removed")
+)
+old_symbols["security"].fillna(old_symbols["security_removed"], inplace=True)
+old_symbols.drop(columns=["security_removed"], inplace=True)
 
-all_symbols = symbols.merge(old_symbols, on=['symbol'], how='outer', suffixes=('', '_old'))
-all_symbols['security'].fillna(all_symbols['security_old'], inplace=True)
-all_symbols.drop(columns=['security_old'], inplace=True)
-all_symbols['date_added'].fillna(all_symbols['date_added_old'], inplace=True)
-all_symbols.drop(columns=['date_added_old'], inplace=True)
+all_symbols = symbols.merge(
+    old_symbols, on=["symbol"], how="outer", suffixes=("", "_old")
+)
+all_symbols["security"].fillna(all_symbols["security_old"], inplace=True)
+all_symbols.drop(columns=["security_old"], inplace=True)
+all_symbols["date_added"].fillna(all_symbols["date_added_old"], inplace=True)
+all_symbols.drop(columns=["date_added_old"], inplace=True)
 
+# Keep only symbols added after 2000
+all_symbols = all_symbols[all_symbols["date_added"] >= "2000-01-01"]
 
 dtypes = {
     "symbol": pd.StringDtype(),
     "security": pd.StringDtype(),
     "gics_sector": pd.CategoricalDtype(),
-    "gics_sub-industry": pd.CategoricalDtype(),
+    "gics_sub_industry": pd.CategoricalDtype(),
     "headquarters_location": pd.CategoricalDtype(),
     "cik": pd.Int64Dtype(),
-    "founded": pd.StringDtype()
+    "founded": pd.StringDtype(),
 }
 
 all_symbols = all_symbols.astype(dtypes)
